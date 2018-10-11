6133b25f1f4ae59f3fa590afbe7319c7235fed4b
ewhaqh9909 <qhdud0921@naver.com>
2018-10-11 15:46:56 +0900

    05_3

--- 05_1.c
+++ 05_1.c
@@ -4,17 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	char ch;
 	
-	int a;
-	printf("정수 하나를 입력하시오:");
-	scanf("%d",&a);
-	 
-	if(a<0)
+	printf("input a char:");
+	scanf("%c",&ch);
+	
+	if('0'<=ch&&ch<='9')
 	{
-		printf("절대값은 %d 입니다.",-a);
-    }else
-    {
-    	printf("절대값은 %d 입니다.",a);
-	}
+		printf("숫자입니다.");
+	 }else if('A'<=ch&&ch<='Z')
+	 {
+	 	printf("대문자입니다.");
+	 }else if('a'<=ch&&ch<='z')
+	 {
+	 	printf("소문자입니다.");
+	 }else
+	 {
+	 	printf("기타 글자입니다.");
+	  } 
 	return 0;
 }
