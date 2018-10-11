b10f7265a88554f76180976a3783a393b41495f5
ewhaqh9909 <qhdud0921@naver.com>
2018-10-11 16:14:40 +0900

    05_4

--- 05_1.c
+++ 05_1.c
@@ -4,23 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char ch;
+	char c;
+	char str[100];//string
+	int a;
+	a=0;
+	printf("input a string:");
 	
-	printf("input a char:");
-	scanf("%c",&ch);
 	
-	if('0'<=ch&&ch<='9')
-	{
-		printf("숫자입니다.");
-	 }else if('A'<=ch&&ch<='Z')
-	 {
-	 	printf("대문자입니다.");
-	 }else if('a'<=ch&&ch<='z')
-	 {
-	 	printf("소문자입니다.");
-	 }else
-	 {
-	 	printf("기타 글자입니다.");
-	  } 
+    while((c=getchar())!='\n')
+    {
+    	if('0'<=c&&c<='9')
+    	{
+    		a+=1;
+		}else{
+			a+=0;
+		}
+	}
+	printf("The number of digits is %d",a);
+    
 	return 0;
 }
